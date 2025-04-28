51f1bc65881cfaed6de610406d61cc6badb70701
Giang Nguyen <hoanggia@uni-bremen.de>
2025-04-28 16:49:34 +0200

    Fix folder

--- setup.bat
+++ setup.bat
@@ -23,7 +23,6 @@ set "EXT_DIR=%CD%\ext"
 set "ZIP_DIR=%EXT_DIR%\7zip"
 set "ZIP_EXECUTABLE=%ZIP_DIR%\7za.exe"
 if not exist "%ZIP_DIR%" (
-  mkdir "%ZIP_DIR%"
   set "ZIP_FILE=7z2409-extra.7z"
   powershell -NoProfile -Command "C:\Windows\System32\curl.exe --ssl-no-revoke -L -o %EXT_DIR%\7zr.exe https://www.7-zip.org/a/7zr.exe"
   powershell -NoProfile -Command "C:\Windows\System32\curl.exe --ssl-no-revoke -L -o %EXT_DIR%\!ZIP_FILE! https://www.7-zip.org/a/!ZIP_FILE!"
